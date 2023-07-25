44916d4f08e834d74064866e7f3a4104c0e96710
Achref LAABIDI <abidi.achref95@gmail.com>
2023-07-26 00:27:53 +0100

    added tailwind and daisyui

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,7 +1,7 @@
 const ProductTable = ({ products }) => {
     return (
-      <div>
-        <table>
+      <div class="overflow-x-auto mr-32 ml-32">
+        <table class="table">
           <thead>
             <tr style={{backgroundColor: 'lightgrey'}}>
               <th>Name</th>
@@ -10,7 +10,7 @@ const ProductTable = ({ products }) => {
           </thead>
           <tbody>
             {products.map((product) => (
-            <tr key={product.id} style={{ color: product.inStock ? 'black' : 'red' }}>
+            <tr class="hover" key={product.id} style={{ color: product.inStock ? 'black' : 'red' }}>
                 <td>{product.name}</td>
                 <td>{product.price}</td>
               </tr>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,22 @@
 const SearchBar = ({ handleSearch, filteredProducts, inStock }) => {
     return (
         <div>
-        <h6>Search</h6>
+        <h6 class="mb-2">Search</h6>
         <form>
             <input 
+                class="input input-bordered input-primary w-full max-w-xs" 
                 type="text" 
                 placeholder="Search for products"
                 onChange={(e) => handleSearch(e.target.value)}
             />
-            <input 
-                type="checkbox"
-                onChange={(e) => filteredProducts(e.target.value) }
-                checked={inStock}
-            />
-            <p>Only show products in stock</p>
+            <div class="flex justify-center">
+                <input 
+                    type="checkbox"
+                    onChange={(e) => filteredProducts(e.target.value) }
+                    checked={inStock}
+                />
+                <p class="ml-2">Only show products in stock</p>
+            </div>
         </form>
         </div>
         
