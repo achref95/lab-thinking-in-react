38714eb6e154352e8ac9cef6b0f861b15514a252
Achref LAABIDI <abidi.achref95@gmail.com>
2023-07-26 01:42:27 +0100

    done with all iterations

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,18 +4,18 @@ const SearchBar = ({ handleSearch, filteredProducts, inStock }) => {
         <h6 class="mb-2">Search</h6>
         <form>
             <input 
-                class="input input-bordered input-primary w-full max-w-xs" 
+                class="input input-bordered input-primary w-full max-w-xs mb-6" 
                 type="text" 
                 placeholder="Search for products"
                 onChange={(e) => handleSearch(e.target.value)}
             />
-            <div class="flex justify-center">
+            <div class="flex justify-center mb-6">
                 <input 
                     type="checkbox"
                     onChange={(e) => filteredProducts(e.target.value) }
                     checked={inStock}
                 />
-                <p class="ml-2">Only show products in stock</p>
+                <p class="ml-2 ">Only show products in stock</p>
             </div>
         </form>
         </div>
